236d13c2b7b97560a6b4211f09c9d478516b5834
OmerTzadok <omerzadok@gmail.com>
2022-01-09 18:38:04 +0200

    New add

---
+++ DerivativeCalc.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+def Derivative (df,yAxis,xAxis,derivativeName,multiplier):
+    diff_df = df.diff()
+    diff_df = diff_df.iloc[1: , :].reset_index()
+
+    df[yAxis+'_diff'] = diff_df[yAxis]
+    df[xAxis+'_diff'] = diff_df[xAxis]
+    
+    df.loc[df['Edisk/V_diff'] > 0, 'E_direction'] = 1
+    df.loc[df['Edisk/V_diff'] < 0, 'E_direction'] = -1
+
+    df[derivativeName+'_abs'] = df.apply( lambda row: abs((row[yAxis+'_diff'] / row[xAxis+'_diff'])*row[multiplier]) , axis=1)
+    df[derivativeName] = df.apply( lambda row: (row[yAxis+'_diff'] / row[xAxis+'_diff'])*row[multiplier] , axis=1)
+    
+    return(df)
