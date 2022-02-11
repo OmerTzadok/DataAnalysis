e0ca3f6de78f80e037efce2f6470573358c5623e
OmerTzadok <omerzadok@gmail.com>
2022-02-11 13:11:13 +0200

    ProbPlot rev2

--- Services/ProbPlot.py
+++ Services/ProbPlot.py
@@ -1,11 +1,11 @@
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import probscale
 from numpy import min as npmin
 from numpy import max as npmax
 
 def NormProbPlot (df,valuecolumn,probaxis):
     
-    fig, ax = pyplot.subplots()
+    fig, ax = plt.subplots()
     common_opts = dict(
         probax=probaxis,
         datalabel=valuecolumn,
@@ -20,8 +20,11 @@ def NormProbPlot (df,valuecolumn,probaxis):
     if probaxis == 'y':
         ax.set_xlim(left=npmin(df[valuecolumn]), right=npmax(df[valuecolumn]))
         ax.set_ylim(bottom=0.01, top=99.99)
+        plt.yticks([0.1,0.5,5,25,50,75,95,99.5,99.9])
     if probaxis == 'x':
         ax.set_ylim(bottom=npmin(df[valuecolumn]), top=npmax(df[valuecolumn]))
         ax.set_xlim(left=0.01, right=99.99)
-        
-    fig.tight_layout()
+        plt.xticks([0.1,0.5,5,25,50,75,95,99.5,99.9],rotation=45)
+    
+    plt.grid()
+    fig.show()
